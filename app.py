f33d2d46c76a78e5d77e337aab821004309d3a28
Rik Baselier <r.baselier@student.fontys.nl>
2022-11-03 20:11:10 +0100

    nav & menu bar for menu page

--- app.py
+++ app.py
@@ -5,3 +5,19 @@ app = Flask(__name__)
 @app.route("/")
 def homepage():
     return render_template("index.html")
+
+@app.route("/menu")
+def menu():
+    return render_template("menu.html")
+
+@app.route("/pizzas", methods=["GET"])
+def pizzas():
+    return render_template("pizzas.html")
+
+@app.route("/drinks", methods=["GET"])
+def drinks():
+    return render_template("drinks.html")
+
+@app.route("/desserts", methods=["GET"])
+def desserts():
+    return render_template("desserts.html")
