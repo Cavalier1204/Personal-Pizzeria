f7695b9520e7919d84dbbab9164156655632338e
Rik Baselier <r.baselier@student.fontys.nl>
2022-11-14 15:25:46 +0100

    pizza menu list is 'automated'

--- app.py
+++ app.py
@@ -4,6 +4,72 @@ app = Flask(__name__)
 
 order = []
 
+listOfPizzas = [
+    { # Margherita
+        "id" : 1,
+        "name" : "Margherita",
+        "ingredients" : "Tomato sauce, mozzarella",
+        "price" : 4.0,
+        "image" : "pizza.png"
+    },
+    { # Pepperoni
+        "id" : 2,
+        "name" : "Pepperoni",
+        "ingredients" : "Tomato sauce, mozzarella, pepperoni",
+        "price" : 5.0,
+        "image" : "pizza.png"
+    },
+    { # BBQ Chicken
+        "id" : 3,
+        "name" : "BBQ Chicken",
+        "ingredients" : "Tomato sauce, mozzarella, chicken, BBQ sauce",
+        "price" : 6.5,
+        "image" : "pizza.png"
+    },
+    { # Shoarma
+        "id" : 4,
+        "name" : "Shoarma",
+        "ingredients" : "Tomato sauce, mozzarella, chicken shoarma, garlic sauce",
+        "price" : 6.5,
+        "image" : "pizza.png"
+    },
+    { # Prosciutto
+        "id" : 5,
+        "name" : "Prosciutto",
+        "ingredients" : "Tomato sauce, parmesan cheese, prosciutto crudo, arugula",
+        "price" : 5.5,
+        "image" : "pizza.png"
+    },
+    { # Calzone
+        "id" : 6,
+        "name" : "Calzone",
+        "ingredients" : "Tomato sauce, mozzarella, salami, folded in",
+        "price" : 6.5,
+        "image" : "pizza.png"
+    },
+    { # Contadina
+        "id" : 7,
+        "name" : "Contadina",
+        "ingredients" : "Tomato sauce, mozzarella, cherry tomatoes, arugula",
+        "price" : 5.5,
+        "image" : "pizza.png"
+    },
+    { # Nutella
+        "id" : 8,
+        "name" : "Nutella",
+        "ingredients" : "Nutella, powdered sugar",
+        "price" : 4.0,
+        "image" : "pizza.png"
+    },
+    { # Vegan
+        "id" : 9,
+        "name" : "Vegan",
+        "ingredients" : "Tomato sauce, vegan mozzarella, cherry tomatoes",
+        "price" : 4.5,
+        "image" : "pizza.png"
+    },
+]
+
 @app.route("/")
 def homepage():
     return render_template("index.html")
@@ -24,7 +90,7 @@ def cartcontent():
 
 @app.route("/pizzas", methods=["GET", "POST"])
 def pizzas():
-    return render_template("pizzas.html")
+    return render_template("pizzas.html", listOfPizzas = listOfPizzas)
 
 @app.route("/drinks", methods=["GET"])
 def drinks():
